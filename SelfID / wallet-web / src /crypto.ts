037204042aea52987b881767b8087cdaed0203d2
wminzel <132092462+wminzel@users.noreply.github.com>
2025-12-11 04:58:11 -0600

    Delete SelfID / wallet-web / src  directory

--- SelfID / wallet-web / src /crypto.ts
+++
@@ -1,6 +0,0 @@
-export function generateDID(): string {
-  const array = new Uint8Array(32);
-  crypto.getRandomValues(array);
-  const hex = Array.from(array, b => b.toString(16).padStart(2, "0")).join("");
-  return `did:self:${hex}`;
-}
